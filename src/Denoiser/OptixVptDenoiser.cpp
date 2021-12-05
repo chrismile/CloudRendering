f4082e5aa9a5164cb7965b5cd421c5c0cfce6f3c
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2021-12-05 23:38:10 +0100

    Fixed build errors on Windows.

--- src/Denoiser/OptixVptDenoiser.cpp
+++ src/Denoiser/OptixVptDenoiser.cpp
@@ -26,9 +26,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <optix_stubs.h>
-#include <optix_function_table_definition.h>
-
 #include <Utils/File/Logfile.hpp>
 #include <Graphics/Vulkan/Utils/Swapchain.hpp>
 #include <Graphics/Vulkan/Render/CommandBuffer.hpp>
@@ -40,6 +37,9 @@
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 #endif
+
+#include <optix_stubs.h>
+#include <optix_function_table_definition.h>
 
 static void _checkOptixResult(OptixResult result, const std::string& text, const std::string& locationText) {
     if (result != OPTIX_SUCCESS) {
