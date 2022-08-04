684800723c87da3f092f2b5466926fd0d0938d4c
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2022-08-04 13:01:58 +0200

    Added missing Vulkan device extensions.

--- src/Main.cpp
+++ src/Main.cpp
@@ -68,7 +68,7 @@ int main(int argc, char *argv[]) {
     sgl::Window* window = sgl::AppSettings::get()->createWindow();
 
     std::vector<const char*> optionalDeviceExtensions;
-#ifdef SUPPORT_OPTIX
+#ifdef SUPPORT_CUDA_INTEROP
     optionalDeviceExtensions = sgl::vk::Device::getCudaInteropDeviceExtensions();
 #endif
     optionalDeviceExtensions.push_back(VK_KHR_TIMELINE_SEMAPHORE_EXTENSION_NAME);
