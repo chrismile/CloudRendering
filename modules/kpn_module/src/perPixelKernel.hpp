fe8e262efb0147ef3205b2aff3f05fbca4224096
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2024-03-15 01:01:04 +0100

    Fixed KPN module build on Windows.

--- modules/kpn_module/src/perPixelKernel.hpp
+++ modules/kpn_module/src/perPixelKernel.hpp
@@ -32,8 +32,10 @@
 #include <torch/script.h>
 #include <torch/types.h>
 
-torch::Tensor getInvIterBaseCuda(torch::Tensor x, torch::Tensor low_res_pred, torch::Tensor low_res_x, torch::Tensor prev, torch::Tensor fused_weights, bool usePrevious, int kernelSize);
-torch::Tensor perPixelKernelForward(torch::Tensor image, torch::Tensor weights, int64_t kernelSize);
-torch::Tensor perPixelKernelCuda(torch::Tensor image, torch::Tensor weights, int64_t kernelSize);
+KPN_MODULE_OP_API torch::Tensor getInvIterBaseCuda(
+        torch::Tensor x, torch::Tensor low_res_pred, torch::Tensor low_res_x, torch::Tensor prev,
+        torch::Tensor fused_weights, bool usePrevious, int kernelSize);
+KPN_MODULE_OP_API torch::Tensor perPixelKernelForward(torch::Tensor image, torch::Tensor weights, int64_t kernelSize);
+KPN_MODULE_OP_API torch::Tensor perPixelKernelCuda(torch::Tensor image, torch::Tensor weights, int64_t kernelSize);
 
 #endif //PER_PIXEL_KERNEL_HPP
