3256586d1fab4e921dd1d8c8d39fde332719e2b4
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2024-03-27 05:34:54 +0100

    Added missing includes.

--- src/MainAppState.cpp
+++ src/MainAppState.cpp
@@ -26,7 +26,9 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <fstream>
 #include <json/json.h>
+#include <Utils/File/Logfile.hpp>
 
 #include "MainApp.hpp"
 
