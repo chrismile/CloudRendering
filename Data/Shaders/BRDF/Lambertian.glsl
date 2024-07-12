0fcf699912cd1c97bfb43f00188dce853e199a81
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2024-07-12 21:11:44 +0200

    Update Lambertian.glsl

--- Data/Shaders/BRDF/Lambertian.glsl
+++ Data/Shaders/BRDF/Lambertian.glsl
@@ -32,62 +32,64 @@
 
 // ---------- BRDF Interface ----------
 
-// Compute Importance Sampled light vector
+// Compute importance-sampled light vector
 vec3 sampleBrdf(mat3 frame, flags hitFlags) {
     hitFlags.specularHit = false;
     hitFlags.clearcoatHit = false;
     
-    #ifdef UNIFORM_SAMPLING
+#ifdef UNIFORM_SAMPLING
     
     // Sampling PDF: 1/(2pi)
     return frame * sampleHemisphere(vec2(random(), random()));
     
-    #else
+#else
     
     // Sampling PDF: cos(theta) / pi
     return frame * sampleHemisphereCosineWeighted(vec2(random(), random()));
     
-    #endif
+#endif
 }
 
-// Evaluate BRDF with compensation of Importance Sampling and a fixed sampling PDF
+// Evaluate BRDF with compensation of importance sampling and a fixed sampling PDF
 vec3 evaluateBrdf(vec3 viewVector, vec3 lightVector, vec3 normalVector, vec3 isoSurfaceColor) {
     float theta = dot(lightVector, normalVector);
-    #ifdef UNIFORM_SAMPLING
+
+    // BRDF: R/pi = isosurfaceColor / pi, i.e., sampling PDF is multiplied with pi.
+#ifdef UNIFORM_SAMPLING
     
     // Sampling PDF: 1/(2pi)
-    float pdf = 0.5;
+    float pdfFactor = 0.5;
     
-    #else
+#else
     
     // Sampling PDF: cos(theta) / pi
-    float pdf = theta;
+    float pdfFactor = theta;
 
-    #endif
+#endif
 
-    return (isoSurfaceColor * theta) / pdf;
+    return (isoSurfaceColor * theta) / pdfFactor;
 }
 
 vec3 evaluateBrdfNee(vec3 viewVector, vec3 dirOut, vec3 dirNee, vec3 normalVector, vec3 tangentVector, vec3 bitangentVector, vec3 isoSurfaceColor, bool useMIS, float samplingPDF, flags hitFlags, out float pdfSamplingOut, out float pdfSamplingNee) {
     float thetaNee = dot(normalVector, dirNee);
-    #ifdef UNIFORM_SAMPLING
-        pdfSamplingNee;
-        if(useMIS) {
-            pdfSamplingNee = 1.0 / (2.0 * M_PI);
-        }
-        pdfSamplingOut = 1.0 / (2.0 * M_PI);
-    #else
-        pdfSamplingNee;
-        if(useMIS) {
-            pdfSamplingNee = thetaNee / M_PI;
-        }
-        pdfSamplingOut = dot(dirOut, normalVector) / M_PI;
-    #endif
+#ifdef UNIFORM_SAMPLING
+    pdfSamplingNee;
+    if(useMIS) {
+        pdfSamplingNee = 1.0 / (2.0 * M_PI);
+    }
+    pdfSamplingOut = 1.0 / (2.0 * M_PI);
+#else
+    pdfSamplingNee;
+    if(useMIS) {
+        pdfSamplingNee = thetaNee / M_PI;
+    }
+    pdfSamplingOut = dot(dirOut, normalVector) / M_PI;
+#endif
     
     return isoSurfaceColor / M_PI * thetaNee;
 }
 
-// Combined Call to importance sample and evaluate BRDF
+// Combined call to importance sample and evaluate BRDF
 
 vec3 computeBrdf(vec3 viewVector, out vec3 lightVector, vec3 normalVector, vec3 tangentVector, vec3 bitangentVector, mat3 frame, vec3 isoSurfaceColor, out flags hitFlags, out float samplingPDF) {
     lightVector = sampleBrdf(frame, hitFlags);
