96fff9df18b7020d37a5226b038410d4fd945749
JI20 <jonas.itt@tum.de>
2024-08-16 10:14:26 +0200

    Reset default values

--- src/PathTracer/VolumetricPathTracingPass.hpp
+++ src/PathTracer/VolumetricPathTracingPass.hpp
@@ -355,15 +355,15 @@ private:
     std::string pytorchDenoiserModelFilePath; //< Delayed loading.
 
     // Isosurface data.
-    bool useIsosurfaces = true;
-    float isoValue = 0.3f;
+    bool useIsosurfaces = false;
+    float isoValue = 0.5f;
     float isoStepWidth = 0.25f;
     float maxAoDist = 0.05f;
     int numAoSamples = 4;
     bool useAoDist = false;
     glm::vec3 isosurfaceColor = glm::vec3(0.4f, 0.4f, 0.4f);
     IsosurfaceType isosurfaceType = IsosurfaceType::DENSITY;
-    SurfaceBrdf surfaceBrdf = SurfaceBrdf::DISNEY;
+    SurfaceBrdf surfaceBrdf = SurfaceBrdf::LAMBERTIAN;
     bool useIsosurfaceTf = false;
     float minGradientVal = 0.0f, maxGradientVal = 1.0f;
     int numIsosurfaceSubdivisions = 2;
