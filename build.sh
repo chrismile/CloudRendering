8b60791be096744a20df0afe1958a8544bede38a
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2025-06-03 18:19:18 +0200

    Fixed run.sh script for macOS vcpkg build.

--- build.sh
+++ build.sh
@@ -1607,7 +1607,10 @@ fi
 # Create a run script.
 if $use_msys; then
     printf "@echo off\npushd %%~dp0\npushd bin\nstart \"\" CloudRendering.exe\n" > "$destination_dir/run.bat"
-elif $use_macos; then
+elif [ $use_macos = true ] && [ $use_vcpkg = true ]; then
+    printf "#!/bin/sh\npushd \"\$(dirname \"\$0\")\" >/dev/null\n./CloudRendering\npopd\n" > "$destination_dir/run.sh"
+    chmod +x "$destination_dir/run.sh"
+elif [ $use_macos = true ] && [ $use_vcpkg = false ]; then
     printf "#!/bin/sh\npushd \"\$(dirname \"\$0\")\" >/dev/null\n./CloudRendering.app/Contents/MacOS/CloudRendering\npopd\n" > "$destination_dir/run.sh"
     chmod +x "$destination_dir/run.sh"
 else
